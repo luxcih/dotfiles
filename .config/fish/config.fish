35c9b5f8c6330fca57c2c8bcb66f6b0c4c62c3ce
Luxcih <luxcih.me@gmail.com>
2024-04-02 11:27:58 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,9 @@
+set fish_greeting
+
+set -gx TERM    xterm-256color
+set -gx EDITOR  nvim
+set -gx LANG    en_PH.UTF-8
+set -gx LC      en_PH.UTF-8
+
 zoxide init fish --cmd cd | source
 starship init fish | source
