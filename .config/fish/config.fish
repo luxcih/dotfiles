9cca9108a0a0721efcb89d0d8d2e1d6f59d0a2c0
Luxcih <luxcih.me@gmail.com>
2024-04-02 11:50:58 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
+if status is-interactive
+and not set -q TMUX
+    if tmux has-session
+        exec tmux attach-session
+    else
+        exec tmux new-session
+    end
+end
+
 set fish_greeting
 
 set -gx TERM    xterm-256color
