a4a7fe02f4ffe9d179ff038524f280f10d37f9f0
Luxcih <luxcih.me@gmail.com>
2024-05-19 16:51:21 +0800

    Update nvim config

--- .config/nvim/lua/user/config.lua
+++ .config/nvim/lua/user/config.lua
@@ -1,33 +1,45 @@
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_python3_provider = 0
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_node_provider = 0
-vim.g.loaded_perl_provider = 0
+-- Disable some built-in plugins we don't need
+vim.g.loaded_netrw = 1         -- Disable netrw
+vim.g.loaded_netrwPlugin = 1   -- Disable netrwPlugin
+vim.g.loaded_python3_provider = 0 -- Disable Python 3 provider
+vim.g.loaded_ruby_provider = 0 -- Disable Ruby provider
+vim.g.loaded_node_provider = 0 -- Disable Node.js provider
+vim.g.loaded_perl_provider = 0 -- Disable Perl provider
 
-vim.opt.expandtab = true
-vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+-- Indentation settings
+vim.opt.expandtab = true       -- Use spaces instead of tabs
+vim.opt.smartindent = true     -- Enable smart indentation
+vim.opt.tabstop = 4            -- Number of spaces per tab
+vim.opt.softtabstop = 4        -- Number of spaces per tab in insert mode
+vim.opt.shiftwidth = 4         -- Number of spaces to use for each step of (auto)indent
 
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "both"
+-- Cursor settings
+vim.opt.cursorline = true      -- Highlight the screen line of the cursor
+vim.opt.cursorlineopt = "both" -- Highlight both line and column of the cursor
 
-vim.opt.timeout = true
-vim.opt.timeoutlen = 0
+-- Timing settings
+vim.opt.timeout = true         -- Enable timeout for mapped sequences
+vim.opt.timeoutlen = 0         -- Timeout length for mapped sequences (in milliseconds)
 
-vim.opt.number = true
-vim.opt.undofile = true
-vim.opt.smartcase = true
-vim.opt.termguicolors = true
+-- UI settings
+vim.opt.number = true          -- Show line numbers
+vim.opt.undofile = true        -- Enable persistent undo
+vim.opt.smartcase = true       -- Enable smart case search
+vim.opt.termguicolors = true   -- Enable 24-bit RGB color in the TUI
 
-vim.opt.laststatus = 3
-vim.opt.hlsearch = false
-vim.opt.showmode = false
+vim.opt.laststatus = 3         -- Global statusline
+vim.opt.hlsearch = false       -- Disable search highlighting
+vim.opt.showmode = false       -- Don't show mode (e.g., -- INSERT --)
 
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
-vim.opt.shortmess:append("sI")
-vim.opt.whichwrap:append("<>[]hl")
+-- Completion settings
+vim.opt.completeopt = { "menu", "menuone", "noselect" } -- Completion options
+
+-- Clipboard settings
+vim.opt.clipboard = "unnamedplus" -- Use system clipboard
+
+-- Mouse settings
+vim.opt.mouse = "a" -- Enable mouse in all modes
+
+-- Miscellaneous settings
+vim.opt.shortmess:append("sI")     -- Disable startup messages and ins-completion-menu messages
+vim.opt.whichwrap:append("<>[]hl") -- Allow specified keys to wrap to next/previous line
