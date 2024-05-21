4ac7edc1fe6b82aff9ba78f4ac3172d41cf53fd8
Luxcih <luxcih.me@gmail.com>
2024-05-21 16:03:34 +0800

    Update nvim config

--- .config/nvim/lua/base/plugins/colorscheme.lua
+++ .config/nvim/lua/base/plugins/colorscheme.lua
@@ -3,9 +3,9 @@ return {
         "catppuccin/nvim",
         name = "catppuccin",
         lazy = false,
+        priority = 1000,
         config = function()
             vim.cmd.colorscheme("catppuccin")
         end,
-        priority = 1000,
     },
 }
--- .config/nvim/lua/base/plugins/utils.lua
+++ .config/nvim/lua/base/plugins/utils.lua
@@ -1,4 +1,8 @@
 return {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    { "nvim-lua/plenary.nvim",          event = "VeryLazy" },
+    { "nvim-tree/nvim-web-devicons",    event = "VeryLazy" },
+    { "famiu/bufdelete.nvim",           event = "VeryLazy" },
+    { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
+    { "andweeb/presence.nvim",          event = "VeryLazy", config = true },
 }
