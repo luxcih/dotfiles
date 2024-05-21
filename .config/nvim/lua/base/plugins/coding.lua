ad896115fcbf75d0cb3a5c418603bed9eb2801bc
Luxcih <luxcih.me@gmail.com>
2024-05-21 16:04:02 +0800

    Add nvim coding plugins

---
+++ .config/nvim/lua/base/plugins/coding.lua
@@ -0,0 +1,10 @@
+local event = { "BufNewFile", "BufReadPre", "InsertEnter", "VeryLazy" }
+
+return {
+    { "lewis6991/gitsigns.nvim",             event = event, config = true },
+    { "numToStr/Comment.nvim",               event = event, config = true },
+    { "folke/todo-comments.nvim",            event = event, config = true },
+    { "windwp/nvim-autopairs",               event = event, config = true },
+    { "kylechui/nvim-surround",              event = event, config = true },
+    { "lukas-reineke/indent-blankline.nvim", event = event, config = true,  main = "ibl" },
+}
