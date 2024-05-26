ed487d1f3e8453b5e3a96d40c4ef5b7cad337ebe
Luxcih <luxcih.me@gmail.com>
2024-05-26 10:14:46 +0800

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,5 +15,3 @@ set -gx LC      en_PH.UTF-8
 
 zoxide init fish --cmd cd | source
 starship init fish | source
-
-__wakatime
--- .config/fish/functions/__wakatime.fish
+++
@@ -1,4 +0,0 @@
-function __wakatime --on-event fish_postexec
-    wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "Terminal" --entity (echo $history[1] | cut -d " " -f1) 2>&1 > /dev/null&
-    disown $last_pid
-end
