d926e57a87efdfcfb7ffe3a7c2cb5656cec307cd
Luxcih <luxcih.me@gmail.com>
2024-07-17 11:56:41 +0800

    Update nvim config

--- .config/nvim/lua/config/keymaps/git.lua
+++ .config/nvim/lua/config/keymaps/git.lua
@@ -1,7 +1,21 @@
 local M = {}
 
-M.setup = function ()
-    vim.keymap.set("n", "<Leader>gp", "<Cmd>Gitsigns preview_hunk<CR>")
+M.setup = function()
+    local gitsigns = require("gitsigns")
+    vim.keymap.set("n", "<Leader>hp", gitsigns.preview_hunk)
+    vim.keymap.set("n", "<Leader>hs", gitsigns.stage_hunk)
+    vim.keymap.set("n", "<Leader>hr", gitsigns.reset_hunk)
+    vim.keymap.set("n", "<Leader>hu", gitsigns.undo_stage_hunk)
+    vim.keymap.set(
+        "v",
+        "<leader>hs",
+        function() gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") }) end
+    )
+    vim.keymap.set(
+        "v",
+        "<leader>hr",
+        function() gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") }) end
+    )
 end
 
 return M
