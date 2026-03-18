2d82d1d0f53b839cda205bdd95bba4f281758da3
Luxcih <luxcih.me@gmail.com>
2026-03-18 21:44:36 +0800

    Remove nvim terminal plugin

--- .config/nvim/lua/config/keymaps/terminal.lua
+++
@@ -1,8 +0,0 @@
-local M = {}
-
-M.setup = function ()
-    vim.keymap.set("n", "<Leader>tt", "<Cmd>ToggleTerm<CR>")
-    vim.keymap.set("t", "<Esc>", "<C-\\><C-n><Cmd>ToggleTerm<CR>")
-end
-
-return M
