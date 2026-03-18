d7abb45d57e1e430daff290afd792f3b5477ef16
Luxcih <luxcih.me@gmail.com>
2026-03-18 16:54:51 +0800

    Refactor nvim cmp

--- .config/nvim/lua/config/cmp.lua
+++ .config/nvim/lua/config/cmp.lua
@@ -29,48 +29,19 @@ M.cmp_icons = {
     TypeParameter = "",
 }
 
-M.opts = function(_, opts)
-    opts.sources = opts.sources or {}
-    table.insert(opts.sources, {
-        "lazydev",
-        group_index = 0,
-    })
-end
-
 M.setup = function(plugin, opts)
     local cmp = require("cmp")
 
     -- Border to use for every completion window
     local window_border = cmp.config.window.bordered({ border = "single" })
 
-    local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0
-            and vim.api
-                    .nvim_buf_get_lines(0, line - 1, line, true)[1]
-                    :sub(col, col)
-                    :match("%s")
-                == nil
-    end
-
-    local feedkey = function(key, mode)
-        vim.api.nvim_feedkeys(
-            vim.api.nvim_replace_termcodes(key, true, true, true),
-            mode,
-            true
-        )
-    end
-
     cmp.setup({
-        snippet = {
-            expand = function(args) vim.fn["vsnip#anonymous"](args.body) end,
-        },
         window = {
             completion = window_border,
             documentation = window_border,
         },
         sources = {
+            { name = "lazydev" },
             { name = "nvim_lsp_signature_help" },
             { name = "nvim_lsp" },
             { name = "buffer" },
@@ -88,18 +59,6 @@ M.setup = function(plugin, opts)
             ["<Esc>"] = cmp.mapping.abort(),
             ["<CR>"] = cmp.mapping.confirm(),
             ["<C-Space>"] = cmp.mapping(function() cmp.complete() end, { "i", "s" }),
-            ["<Tab>"] = cmp.mapping(function(fallback)
-                if vim.fn["vsnip#available"](1) == 1 then
-                    feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-            ["<S-Tab>"] = cmp.mapping(function()
-                if vim.fn["vsnip#jumpable"](-1) == 1 then
-                    feedkey("<Plug>(vsnip-jump-prev)", "")
-                end
-            end, { "i", "s" }),
         },
     })
 
