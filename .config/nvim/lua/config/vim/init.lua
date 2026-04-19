6e33bc85fd7c275565d6d3394fb22f5ba4bc346f
Luxcih <luxcih.me@gmail.com>
2026-04-19 21:38:49 +0800

    Enable neovim ui2

--- .config/nvim/lua/config/vim/init.lua
+++ .config/nvim/lua/config/vim/init.lua
@@ -4,6 +4,8 @@ M.keymaps = require("config.keymaps")
 M.options = require("config.vim.options")
 
 M.setup = function()
+    require("vim._core.ui2").enable()
+
     vim.g.mapleader = " "
     vim.g.maplocalleader = " "
 
